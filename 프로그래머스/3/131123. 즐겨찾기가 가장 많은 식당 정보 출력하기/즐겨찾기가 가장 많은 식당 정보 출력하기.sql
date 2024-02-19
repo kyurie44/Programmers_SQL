77290e561e3ca5b7deaf2acb59f319eb16d4cbab
kyurie44 <131845766+kyurie44@users.noreply.github.com>
2024-02-19 21:53:55 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,15 +1,29 @@
 # -- 코드를 입력하세요
-SELECT FOOD_TYPE
-    , REST_ID
-    , REST_NAME
-    , FAVORITES
-FROM REST_INFO 
-WHERE (FOOD_TYPE, FAVORITES) IN (
-                                SELECT FOOD_TYPE
-                                    , MAX(FAVORITES) AS FAVORITES
-                                FROM REST_INFO
-                                GROUP BY FOOD_TYPE
-                                )
-ORDER BY FOOD_TYPE DESC
+# SELECT FOOD_TYPE
+#     , REST_ID
+#     , REST_NAME
+#     , FAVORITES
+# FROM REST_INFO AS A
+# WHERE (FOOD_TYPE, FAVORITES) IN (
+#                                 SELECT FOOD_TYPE
+#                                     , MAX(FAVORITES) AS FAVORITES
+#                                 FROM REST_INFO AS B
+#                                 GROUP BY FOOD_TYPE
+#                                 ) 
+# ORDER BY FOOD_TYPE DESC
+# ;
+SELECT B.FOOD_TYPE
+    , A.REST_ID
+    , A.REST_NAME
+    , B.FAVORITES 
+FROM REST_INFO AS A
+JOIN (
+        SELECT FOOD_TYPE
+            , MAX(FAVORITES) AS FAVORITES
+        FROM REST_INFO
+        GROUP BY FOOD_TYPE
+     ) AS B
+ON A.FOOD_TYPE = B.FOOD_TYPE
+AND A.FAVORITES = B.FAVORITES
+ORDER BY A.FOOD_TYPE DESC
 ;
-     
