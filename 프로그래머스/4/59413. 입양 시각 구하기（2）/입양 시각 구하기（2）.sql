9326b00c7cf64971d8a1a6acf622d9756d05ac6d
kyurie44 <131845766+kyurie44@users.noreply.github.com>
2024-02-22 22:48:27 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,21 @@
+-- 코드를 입력하세요
+WITH A AS (
+        SELECT (@rownum := @rownum + 1) -1 AS HOUR
+        FROM ANIMAL_OUTS
+            , (select @rownum := 0) AS HOUR
+        WHERE @rownum < 24
+        ),
+    B AS (
+        SELECT CAST(DATE_FORMAT(DATETIME, '%H') AS UNSIGNED) AS HOUR
+            , IFNULL(COUNT(*), 0) AS COUNT
+        FROM ANIMAL_OUTS 
+        GROUP BY HOUR
+        ORDER BY HOUR    
+    )
+    
+SELECT A.HOUR
+    , IFNULL(B.COUNT, 0) AS COUNT
+FROM A
+LEFT JOIN B
+ON A.HOUR = B.HOUR
+;
