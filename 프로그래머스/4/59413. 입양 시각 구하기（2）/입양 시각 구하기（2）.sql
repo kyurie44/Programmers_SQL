07bdfd7d4cdacf9c28e4e545ba39d2678515b3fb
kyurie44 <131845766+kyurie44@users.noreply.github.com>
2024-02-23 10:22:26 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,29 +1,34 @@
 -- 코드를 입력하세요
-WITH A AS (
-        SELECT (@rownum := @rownum + 1) -1 AS HOUR
-        FROM ANIMAL_OUTS
-            , (select @rownum := 0) AS HOUR
-        WHERE @rownum < 24
-        ),
-    B AS (
-        SELECT CAST(DATE_FORMAT(DATETIME, '%H') AS UNSIGNED) AS HOUR
-            , IFNULL(COUNT(*), 0) AS COUNT
-        FROM ANIMAL_OUTS 
-        GROUP BY HOUR
-        ORDER BY HOUR    
-    )
+# WITH A AS (
+#         SELECT (@rownum := @rownum + 1) -1 AS HOUR
+#         FROM ANIMAL_OUTS
+#             , (select @rownum := 0) AS HOUR
+#         WHERE @rownum < 24
+#         ),
+#     B AS (
+#         SELECT CAST(DATE_FORMAT(DATETIME, '%H') AS UNSIGNED) AS HOUR
+#             , IFNULL(COUNT(*), 0) AS COUNT
+#         FROM ANIMAL_OUTS 
+#         GROUP BY HOUR
+#         ORDER BY HOUR    
+#     )
     
-SELECT A.HOUR
-    , IFNULL(B.COUNT, 0) AS COUNT
-FROM A
-LEFT JOIN B
-ON A.HOUR = B.HOUR
-;
+# SELECT A.HOUR
+#     , IFNULL(B.COUNT, 0) AS COUNT
+# FROM A
+# LEFT JOIN B
+# ON A.HOUR = B.HOUR
+# ;
 
-# SELECT HOUR, COUNT(ANIMAL_ID) AS COUNT
-# FROM ( SELECT (ROW_NUMBER() OVER () - 1) AS HOUR
-#        FROM ANIMAL_OUTS
-#        LIMIT 24) AS 24HOURS_NUM_TABLE 
-#      LEFT JOIN ANIMAL_OUTS ON HOUR = HOUR(DATETIME)
-# GROUP BY HOUR
-# ORDER BY HOUR
+SELECT HOUR
+    , COUNT(ANIMAL_ID) AS COUNT
+FROM ( 
+       SELECT (ROW_NUMBER() OVER () - 1) AS HOUR
+       FROM ANIMAL_OUTS
+       LIMIT 24
+     ) A
+LEFT JOIN ANIMAL_OUTS B 
+ON A.HOUR = HOUR(B.DATETIME)
+
+GROUP BY A.HOUR
+ORDER BY A.HOUR
