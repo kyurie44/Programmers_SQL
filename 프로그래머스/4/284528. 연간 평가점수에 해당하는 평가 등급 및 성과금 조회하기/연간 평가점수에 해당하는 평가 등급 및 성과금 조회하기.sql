cf20375252e09b43966b74799fdac96b2c09b7be
kyurie44 <131845766+kyurie44@users.noreply.github.com>
2024-03-25 20:25:05 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,21 @@
+-- 코드를 작성해주세요
+SELECT B.EMP_NO
+    , B.EMP_NAME
+    , CASE WHEN SCORE >= 96 THEN 'S'
+            WHEN SCORE >= 90 THEN 'A'
+            WHEN SCORE >= 80 THEN 'B'
+        ELSE 'C'
+       END AS GRADE
+     , CASE WHEN SCORE >= 96 THEN B.SAL*20/100
+            WHEN SCORE >= 90 THEN B.SAL*15/100
+            WHEN SCORE >= 80 THEN B.SAL*10/100
+        ELSE 0
+       END AS BONUS
+FROM HR_EMPLOYEES B
+JOIN (  SELECT EMP_NO, AVG(SCORE) AS SCORE
+        FROM HR_GRADE 
+        GROUP BY EMP_NO
+            ) A
+    USING (EMP_NO)
+ORDER BY EMP_NO
+;
