f940f47ca97d71ecedc0678656951da5ec9f4eac
KYURIE PARK <131845766+kyurie44@users.noreply.github.com>
2024-10-24 12:48:55 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -5,11 +5,11 @@ select date_format(sales_date, '%Y-%m-%d') as sales_date
 from ONLINE_SALE 
 where SALES_DATE like '2022-03%'
 union all
-select date_format(sales_date, '%Y-%m-%d') as offline_date
+select date_format(sales_date, '%Y-%m-%d') as sales_date
      , product_id
      , null as user_id
      , sales_amount
 from OFFLINE_SALE  
 where SALES_DATE like '2022-03%'
 
-order by sales_date, product_id, user_id 
+order by sales_date, product_id, user_id
